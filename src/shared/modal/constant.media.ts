6910fef5011d61613d16bc0f5dfbec010df53813
Данис Иртуганов <danisirtuganov@gmail.com>
2024-06-04 22:20:50 +0300

    fix

--- src/shared/modal/constant.media.ts
+++ src/shared/modal/constant.media.ts
@@ -1,3 +1,3 @@
 export const MEDIA_TABLE = 576
-export const MEDIA_TABLE_DESK = 960
+export const MEDIA_TABLE_DESK = 864
 export const MEDIA_DESK = 1200
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
 			xs: '480px',
 			sm: '576px',
 			md: '768px',
-			lg: '960px',
+			lg: '864px',
 			xl: '1200px',
 			'2xl': '1440px'
 		},
